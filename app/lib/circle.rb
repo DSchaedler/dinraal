773e0ec74952500a67ddbe9c638a73d7cd92880f
deeschaedler <deeschaedler@gmail.com>
2021-10-29 01:28:39 -0400

    Removed spaghetti from circle code

--- app/lib/circle.rb
+++ app/lib/circle.rb
@@ -18,7 +18,9 @@ module Circle
       new_pixel[:x] = new_pixel[:x].floor
       new_pixel[:y] = new_pixel[:y].floor
 
-      pixels << { x: new_pixel[:x], y: new_pixel[:y], w: 1, h: 1, r: r, g: g, b: b, a: a }.solid! unless pixels.include?({ x: new_pixel[:x], y: new_pixel[:y], w: 1, h: 1, r: r, g: g, b: b, a: a }.solid!)
+      possible_pixel = { x: new_pixel[:x], y: new_pixel[:y], w: 1, h: 1, r: r, g: g, b: b, a: a }.solid!
+
+      pixels << possible_pixel unless pixels.include?(possible_pixel)
       angle += 1
     end
 
@@ -49,7 +51,8 @@ module Circle
       pixels[x_min] ||= []
       y_min = y - radius
       while y_min <= y_max
-        pixels[x_min][y_min] = { x: x_min, y: y_min, w: 1, h: 1, r: r, g: g, b: b, a: a }.solid! if args.geometry.point_inside_circle?({ x: x_min, y: y_min }, { x: x, y: y }, radius)
+        possible_pixel = { x: x_min, y: y_min, w: 1, h: 1, r: r, g: g, b: b, a: a }.solid!
+        pixels[x_min][y_min] = possible_pixel if args.geometry.point_inside_circle?({ x: x_min, y: y_min }, { x: x, y: y }, radius)
         y_min += 1
       end
       x_min += 1
