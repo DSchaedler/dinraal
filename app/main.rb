4c6ac23a78378026efb3dd6193324fcecd63af8d
Dee Schaedler <dominicschaedler@gmail.com>
2021-11-12 16:10:29 -0500

    Output in the standard way for Dinraal remove old method

--- app/main.rb
+++ app/main.rb
@@ -1,6 +1,6 @@
 def tick args
   #draw_triangle args, [100, 400], [140, 350], [args.mouse.x, args.mouse.y], { r: 255, a: 50 }
-  triangle({x: 100, y: 400, x2: 140, y2: 350, x3: args.mouse.x, y3: args.mouse.y, path: 'sprites/rick.png', image_width: 300})
+  args.outputs.primitives << triangle({x: 100, y: 400, x2: 140, y2: 350, x3: args.mouse.x, y3: args.mouse.y, path: 'sprites/rick.png', image_width: 300})
 
   args.outputs.debug << args.gtk.framerate_diagnostics_primitives
 end
@@ -29,6 +29,8 @@ def triangle(options = {})
   
   pairs = [[{x: x, y: y}, {x: x2, y: y2}], [{x: x2, y: y2}, {x: x3, y: y3}], [{x: x3, y: y3}, {x: x, y: y}]]
 
+  lines = []
+
   # we want to sweep along the "shorter" axis so that we don't have to compute as much
   # this code can definitely be simplified XD
   if (x_bounds[1] - x_bounds[0]) < (y_bounds[1] - y_bounds[0])
@@ -41,7 +43,7 @@ def triangle(options = {})
       [pair[0].x, pair[1].x].sort
     end
 
-    args.outputs.primitives << x_bounds[0].floor.upto(x_bounds[1].ceil).map do |x|
+    lines << x_bounds[0].floor.upto(x_bounds[1].ceil).map do |x|
       ys = eqns.zip(ranges).map do |eqn, range|
         eqn.call(x) if x.between?(*range)
       end.compact
@@ -59,7 +61,7 @@ def triangle(options = {})
       [pair[0].y, pair[1].y].sort
     end
 
-    args.outputs.primitives << y_bounds[0].floor.upto(y_bounds[1].ceil).map do |y|
+    lines << y_bounds[0].floor.upto(y_bounds[1].ceil).map do |y|
       xs = eqns.zip(ranges).map do |eqn, range|
         eqn.call(y) if y.between?(*range)
       end.compact
@@ -68,56 +70,7 @@ def triangle(options = {})
       { x: x1, y: y, x2: x2, y2: y }.merge(color).line!
     end
   end
-end
-
-def draw_triangle args, p0, p1, p2, color = {}
-
-  all_xs = [p0.x, p1.x, p2.x]
-  x_bounds = all_xs.minmax
-  all_ys = [p0.y, p1.y, p2.y]
-  y_bounds = all_ys.minmax
-  lines = [[p0, p1], [p1, p2], [p2, p0]]
-
-  pairs = [[p0, p1], [p1, p2], [p2, p0]]
-  # we want to sweep along the "shorter" axis so that we don't have to compute as much
-  # this code can definitely be simplified XD
-  if (x_bounds[1] - x_bounds[0]) < (y_bounds[1] - y_bounds[0])
-    # sweep along x axis
-    eqns = pairs.map do |pair|
-      two_point_eq *pair
-    end
-
-    ranges = pairs.map do |pair|
-      [pair[0].x, pair[1].x].sort
-    end
-
-    args.outputs.lines << x_bounds[0].floor.upto(x_bounds[1].ceil).map do |x|
-      ys = eqns.zip(ranges).map do |eqn, range|
-        eqn.call(x) if x.between?(*range)
-      end.compact
-
-      y1, y2 = ys.minmax
-      { x: x, y: y1, x2: x, y2: y2 }.merge(color)
-    end
-  else
-     # sweep along y axis instead
-    eqns = pairs.map do |pair|
-      two_point_eq [pair[0].y, pair[0].x], [pair[1].y, pair[1].x]
-    end
-
-    ranges = pairs.map do |pair|
-      [pair[0].y, pair[1].y].sort
-    end
-
-    args.outputs.lines << y_bounds[0].floor.upto(y_bounds[1].ceil).map do |y|
-      xs = eqns.zip(ranges).map do |eqn, range|
-        eqn.call(y) if y.between?(*range)
-      end.compact
-
-      x1, x2 = xs.minmax
-      { x: x1, y: y, x2: x2, y2: y }.merge(color)
-    end
-  end
+  lines
 end
 
 def two_point_eq p0, p1
