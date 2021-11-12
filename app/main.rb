871d75e8cd7f63fa76742a13acd054710bf34d47
Dee Schaedler <dominicschaedler@gmail.com>
2021-11-12 15:26:34 -0500

    move kota code into main.rb

--- app/main.rb
+++ app/main.rb
@@ -1,55 +1,66 @@
-$gtk.reset
+def tick args
+  draw_triangle args, [100, 200], [120, 250], [300, 100], { g: 255, a: 50 }
+  draw_triangle args, [100, 400], [140, 350], [args.mouse.x, args.mouse.y], { r: 255, a: 50 }
+  draw_triangle args, [300, 400], [340, 350], [340, 400], { b: 255, a: 50 }
 
-require 'app/lib/dinraal.rb'
-
-def make_rt(args)
-  args.render_target(:static_rt).clear_before_render = true
-  args.render_target(:static_rt).primitives << Dinraal.triangle_raster(args.state.tri1)
-
-  args.render_target(:static_rt).primitives << Dinraal.circle_outline(x: 20, y: 580, radius: 20, g: 255)
-  args.render_target(:static_rt).primitives << Dinraal.circle_raster(x: 70, y: 580, radius: 20, b: 255)
-
-  args.render_target(:static_rt).primitives << Dinraal.triangle_outline(args.state.tri1)
-  args.render_target(:static_rt).primitives << Dinraal.triangle_outline(args.state.tri2)
-
-  args.render_target(:static_rt).primitives << Dinraal.triangle_center(args.state.tri1).merge(w: 5, h: 5, g: 255).solid!
-  args.render_target(:static_rt).primitives << Dinraal.triangle_center(args.state.tri2).merge(w: 5, h: 5, g: 255).solid!
-
-  args.render_target(:static_rt).primitives << Dinraal.triangle_bounding_box(args.state.tri1)
-  args.render_target(:static_rt).primitives << Dinraal.triangle_bounding_box(args.state.tri2)
+  args.outputs.debug << args.gtk.framerate_diagnostics_primitives
 end
 
-def tick(args)
-  # args.state.tri1 ||= { x: 800, y: 500, x2: 450, y2: 650, x3: 400, y3: 300, path: 'sprites/rick.png', image_width: 300 }
-  args.state.tri1 ||= { x: 800, y: 500, x2: 450, y2: 650, x3: 400, y3: 300, g: 255 }
-  args.state.tri2 ||= { x: 200, y: 600, x2: 400, y2: 600, x3: 275, y3: 500, r: 255 }
-
-  new_tri = args.state.tri1.merge(x: args.inputs.mouse.x, y: args.inputs.mouse.y)
-
-  if args.state.tick_count.zero?
-    make_rt(args)
-  elsif args.inputs.mouse.up && new_tri != args.state.tri1
-    args.state.tri1 = new_tri
-    make_rt(args)
-  else
-    args.render_target(:static_rt).clear_before_render = false
+def draw_triangle args, p0, p1, p2, color = {}
+  args.outputs.borders << [p0, p1, p2].map do |p|
+    {
+      x: p.x - 4, y: p.y - 4, w: 8, h: 8
+    }
   end
 
-  outputs = []
-  outputs << { x: 0, y: 0, w: 1280, h: 720, path: :static_rt }.sprite!
+  all_xs = [p0.x, p1.x, p2.x]
+  x_bounds = all_xs.minmax
+  all_ys = [p0.y, p1.y, p2.y]
+  y_bounds = all_ys.minmax
+  lines = [[p0, p1], [p1, p2], [p2, p0]]
 
-  mouse_x = args.inputs.mouse.x
-  mouse_y = args.inputs.mouse.y
+  pairs = [[p0, p1], [p1, p2], [p2, p0]]
+  # we want to sweep along the "shorter" axis so that we don't have to compute as much
+  # this code can definitely be simplified XD
+  if (x_bounds[1] - x_bounds[0]) < (y_bounds[1] - y_bounds[0])
+    # sweep along x axis
+    eqns = pairs.map do |pair|
+      two_point_eq *pair
+    end
 
-  mouse_inside = Dinraal.point_inside_triangle?(point: { x: mouse_x, y: mouse_y }, triangle: args.state.tri1)
+    ranges = pairs.map do |pair|
+      [pair[0].x, pair[1].x].sort
+    end
 
-  outputs << { x: args.grid.center_x, y: 720, text: "Mouse inside image: #{mouse_inside}", alignment_enum: 1 }.label!
+    args.outputs.lines << x_bounds[0].floor.upto(x_bounds[1].ceil).map do |x|
+      ys = eqns.zip(ranges).map do |eqn, range|
+        eqn.call(x) if x.between?(*range)
+      end.compact
 
-  two_contains_one = Dinraal.triangle_inside_triangle?(inner: args.state.tri2, outer: args.state.tri1)
-  outputs << { x: args.grid.center_x, y: 700, text: "Image contains red triangle: #{two_contains_one}", alignment_enum: 1 }.label!
+      y1, y2 = ys.minmax
+      { x: x, y: y1, x2: x, y2: y2 }.merge(color)
+    end
+  else
+     # sweep along y axis instead
+    eqns = pairs.map do |pair|
+      two_point_eq [pair[0].y, pair[0].x], [pair[1].y, pair[1].x]
+    end
 
-  args.outputs.primitives << outputs
+    ranges = pairs.map do |pair|
+      [pair[0].y, pair[1].y].sort
+    end
 
-  # Optional Debug Information. Uncomment to show
-  # args.outputs.debug << args.gtk.framerate_diagnostics_primitives
+    args.outputs.lines << y_bounds[0].floor.upto(y_bounds[1].ceil).map do |y|
+      xs = eqns.zip(ranges).map do |eqn, range|
+        eqn.call(y) if y.between?(*range)
+      end.compact
+
+      x1, x2 = xs.minmax
+      { x: x1, y: y, x2: x2, y2: y }.merge(color)
+    end
+  end
+end
+
+def two_point_eq p0, p1
+  -> (x) { (p1.y - p0.y) / (p1.x - p0.x) * (x - p0.x) + p0.y }
 end
